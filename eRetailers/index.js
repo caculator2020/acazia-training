ddb4871d877710e00132a7dadce8b4e9ea63f226
Tshady <kullnobita69@gmail.com>
2021-01-22 15:24:44 +0700

    eRetailers complete

--- eRetailers/index.js
+++ eRetailers/index.js
@@ -1,26 +1,32 @@
-const carousel = (carousel) => {
-  const wrapper = carousel.children[0];
-  const dots = carousel.children[1].firstElementChild.children;
-
-  let currentActiveIndex = 0;
-  let curretLeft = 0;
-  dots[currentActiveIndex].classList.toggle("carousel-dot--active");
-
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].addEventListener("click", () => {
-      const swipeLeft = (i - currentActiveIndex) * 375;
-      curretLeft = curretLeft - swipeLeft;
-      wrapper.style.left = curretLeft + "px";
-      dots[currentActiveIndex].classList.toggle("carousel-dot--active");
-      dots[i].classList.toggle("carousel-dot--active");
-      currentActiveIndex = i;
-    });
-  }
-};
-
 window.onload = () => {
-  const carouselItems = document.querySelectorAll(".carousel");
-  for (let i = 0; i < carouselItems.length; i++) {
-    carousel(carouselItems[i]);
-  }
+  $(".slick").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+    ],
+  });
+  $(".slick-3").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+    ],
+  });
 };
